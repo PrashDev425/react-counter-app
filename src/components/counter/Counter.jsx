188b0288a50e764bcfb818e66cc9cf5c09878a91
Prashant Bhandari <bhandariprashant128@gmail.com>
2025-07-15 16:56:21 +0545

    feat(history): recent 5 action history feature completed

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -2,10 +2,11 @@ import { useCounter } from '../../contexts/CounterProvider';
 import upicon from '../../assets/up.svg';
 import downicon from '../../assets/down.svg';
 import reseticon from '../../assets/reset.svg';
+import History from '../history/History';
 import './Counter.css';
 
 function Counter() {
-  const { count, inputValue, animate, setInputValue, counterUp, counterDown, handleReset, handleSetValue } = useCounter();
+  const { count, inputValue, animate, history, setInputValue, counterUp, counterDown, handleReset, handleSetValue } = useCounter();
 
   return (
     <div className="container">
@@ -16,9 +17,9 @@ function Counter() {
           <div className="bar"></div>
         </div>
         <div className="controls">
-          <button className="btn increment" onClick={() => counterUp()}> <img src={upicon} alt=""/></button>
-          <button className="btn reset" onClick={handleReset}> <img src={reseticon} alt=""/></button>
-          <button className="btn decrement" onClick={() => counterDown()}> <img src={downicon} alt=""/></button>
+          <button className="btn increment" onClick={() => counterUp()}> <img src={upicon} alt="" /></button>
+          <button className="btn reset" onClick={handleReset}> <img src={reseticon} alt="" /></button>
+          <button className="btn decrement" disabled={count === 0} onClick={() => counterDown()}> <img src={downicon} alt="" /></button>
         </div>
         <div className="custom-input">
           <label htmlFor="set-value">Set value:</label>
@@ -33,6 +34,7 @@ function Counter() {
             <button className="add-btn" onClick={handleSetValue}>Set</button>
           </div>
         </div>
+        <History history={history} />
       </div>
       <div className="footer">
         Click any button to interact with the counter
